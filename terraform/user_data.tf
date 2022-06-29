67c45ee7751d3da4fdd4370b69df2f16bf68a344
Santiago Botto <sanbotto@gmail.com>
2022-06-28 23:05:12 -0300

    User data improvements

--- terraform/user_data.tf
+++ terraform/user_data.tf
@@ -19,13 +19,12 @@ data "template_file" "ghost_instance" {
 		mysql_user     = var.mysql_user
 		mysql_password = random_password.mysql_password.result
 
-		ghost_email    = var.ghost_email
+		admin_email    = var.admin_email
 		ghost_domain   = var.ghost_domain
-
 		linux_user     = var.linux_user
 		linux_password = random_password.linux_password.result
-
 		project_name   = var.project_name
+		topic_arn      = aws_sns_topic.instance_notifications.arn
 
 		cloudflare_zone_id   = var.cloudflare_zone_id
 		cloudflare_api_token = var.cloudflare_api_token
